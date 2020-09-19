f1b6a7683fdb938639c30ed21bca223d80523000
Sean <sgholson@groupw.com>
2020-09-19 12:43:32 -0400

    Implemented new form LQUBO class into solver and experiment runner

--- examples/experiment_example.py
+++ examples/experiment_example.py
@@ -10,18 +10,17 @@ qap_obj = QAPObjectiveFunction(dat_file='had8.dat',
 tsp_obj = TSPObjectiveFunction(num_points=8)
 
 experiment_config = {
-    'save_csv': True,
+    'save_csv': False,
     'instance': 'tsp',
     'experiment_type': 'iter_lim',
     'num_trials': 2,
     'solver': 'LQUBO',
-    'size': '20',
     'problem_type': 'tsp',
     'max_hd': 11,
     'sampler_type': 'Tabu',
     'objective_function': tsp_obj,
     'num_reads': 10,
-    'num_iters': 30  # Should be a multiple of 5
+    'num_iters': 30
 }
 
 test_experiment = Experiment(**experiment_config)
@@ -36,8 +35,6 @@ n_iters = experiment_stats_result['number_of_iterations']
 
 print('Percent error mean and standard deviation:')
 print('\t{}'.format(pct_err))
-print('Frequency of obtaining optimal mean and standard deviation:')
-print('\t{}'.format(freq_opt))
 print('Timing of code mean and standard deviation:')
 print('\t{}'.format(timing))
 print('Number of iterations mean and standard deviation:')
